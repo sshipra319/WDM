e20d0f62196571072f2c97294d02526a3dd99012
sshipra319 <56846583+sshipra319@users.noreply.github.com>
2020-10-16 20:01:59 -0500

    Add files via upload

---
+++ AgentHome.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+  <title>Home Agent LEAN</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="CSS/leanevent.css">
+  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+    <link rel="icon" href="imagenes/favicon.ico" type="image/x-icon" />
+<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+	<div id="wrapper">
+<header id="header">
+  <nav id="nav-bar">
+    <img src="imagenes\logo-blanco.png" alt="Lean logo" class="hidden-xs" />
+    <div id="logoName">
+      <h3>LEANEVENTO</h3>
+    </div>
+    <ul>
+      <li>
+        <a href="AgentHome.html" id="active">Inicio</a>
+        <a href="VolunteerList.html">Lista de Voluntarios</a>
+        <a href="BusinessList.html">Lista de Fundaciones</a>
+        <a href="AgentListEvent.html">Eventos</a>
+        <a href="AgentProfile.html">Agente</a>        
+      </li>
+    </ul>
+  </nav>
+</header>
+
+<main>
+	<div class= "container-image ">
+    <img src="imagenes\bannerlean1.jpg"/>
+    <img src="imagenes\logo-blanco.png" style="width: 200px;height: 200px; align-content: left;position: absolute;top: 50px; left: 60%;" />
+  </div> 
+	
+</main>
+
+<footer>
+	<div id="bottom"><p style="text-align: center;"><small>Copyright @2019 All rights reserved | This web is made with <i class="fa fa-heart-o"></i> by <span style="color: #FFC300">DiazApps</span></small></p></div>
+</footer>
+
+</div>
+</body>
+</html>
