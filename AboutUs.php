9a820c17ad8eaf5da3a547e09f5716f2a6e365cc
sshipra319 <56846583+sshipra319@users.noreply.github.com>
2020-10-16 20:03:08 -0500

    Add files via upload

---
+++ AboutUs.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="CSS/leanevent.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+    <link rel="icon" href="imagenes/favicon.ico" type="image/x-icon" />
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>About Us</title>
+</head>
+
+<body>
+    <div id="wrapper">
+<header>
+  <nav>
+    <img src="imagenes\logo-blanco.png" alt="Lean logo" class="hidden-xs" />
+    <div id="logoName">
+      <h3>LEANEVENTO</h3>
+    </div>
+    <ul>
+      <li>
+        <a href="Index.html">Inicio</a>
+        <a href="AboutUs" id="active">Quienes Somos</a>
+        <a href="Blog.html">Blog</a>
+        <a href="SignUp.html">Registrate</a>
+        <a href="Contact.php">Contacto</a>
+        <a href="Login.html">Iniciar Sesion</a>
+        <a href="BuyFromUs.html">Comprar Boletos</a>
+      </li>
+    </ul>
+  </nav>
+</header>
+
+<main>
+  <div class= "container-image ">
+    <img src="imagenes\bannerabout.jpg"/>
+    <h1 class="centre"> QUIENES SOMOS </h1>
+        <div class="below"><span style="color: #FFC300;">HOME &nbsp;&nbsp;</span> QUIENES SOMOS </div>
+  </div>
+
+  <div class="wrapper">
+            <aside style="padding-left: 20px;margin-right: 0px;">
+            <p style="color: black; font-size: 20px;"><b>¿CÓMO PUEDES AYUDAR?</b></p>
+            <p><b>PUEDES AYUDAR DE TRES FORMAS:</b></p>
+            <ul class="c" style="margin-left: 10px;padding-left: 20px; text-align: left;">
+                <li>Dona material médico e insumos para Venezuela.</li>
+                <li>A través de donaciones económicas</li>
+                <li>Hazte voluntario.</li>
+            </ul>
+        
+            <p style="color: black; font-size: 20px;"><b>¿DÓNDE ESTAMOS?</b></p>
+            <p><b>SOMOS 17 COORDINACIONES EN ESPAÑA:</b></p>
+            <p>Alicante - Almería - Cataluña - Granada - Islas Canarias - Islas Baleares - León - Madrid -Málaga - Salamanca - Sevilla - Valencia - Valladolid - Zaragoza - LEAN EEUU.</p>
+            
+            <p><b>INSTITUCIONES Y ORGANIZACIONES BENEFICIADAS EN VENEZUELA:</b></p>
+            <p>Ayudamos a 11 Estados a través de 35 puntos de destino:</p>
+
+            <div class="wrapper">
+                <span>
+            <ul class="l" >
+                
+                <li>LEAN Anzoátegui</li>
+                <li>Funsaluz Barcelona, Valencia y Maracaibo</li>
+                <li>Fundación La Pastillita</li>
+                <li>LEAN Aragua 1</li>
+                <li>Parroquia Michelena</li>
+                <li>LEANCaracas1,2,3,4,5,6,7,8y9.</li>
+                <li>Seno Salud</li>
+                <li>Somos Ayuda</li>
+                <li>FDIV</li>
+                <li>Parroq. San Fco. de Asis</li>
+                <li>ONG Pan y Vino</li>
+                <li>LEAN Nueva Esparta</li>
+                <li>Parroquia San Félix</li>
+                <li>Fundación Esperanza de Vida</li>
+                <li>Caritas de Venezuela</li>
+            </ul>
+            </span>            
+            
+                <span id="right">
+                <ul class="l">
+                
+                <li>Fund. Denzel El Guerrero</li>
+                <li>Mensajeras de la Alegría</li>
+                <li>Caritas Valle de la Pascua</li>
+                <li>Caritas Diocesana Barquisimeto</li>
+                <li>Hogar de Niños Impedidos Don Orione</li>
+                <li>AVEPEII</li>
+                <li>Casa Hogar Madre Teresa de Calcuta</li>
+                <li>Seminario Santa Rosa de Lima</li>
+                <li>Casa Aragón</li>
+                <li>Caritas Parraquial de Mérida</li>
+                <li>Asociación Dr. Paúl Moreno Camacho</li>
+                <li>FUNDAYÚDANOS</li>                
+                </ul>
+                </span>
+            </div>
+        </aside>
+
+<p style="color: black; font-size: 20px;"><b>¿Qué es LEAN?</b></p>
+            <p>LEAN es una asociación civil sin fines de lucro conformada por gente con gran sensibilidad social, dedicada a la defensa de los derechos humanos y la consecución de ayuda humanitaria, favoreciendo directamente o a través de otras asociaciones o agrupaciones provinciales a venezolanos residentes en España y a quienes viven en Venezuela.</p>
+        
+    
+            <p style="color: black; font-size: 20px;"><b>¿Cuáles son los fines de LEAN?</b></p>
+            <p>LEAN está dedicada a fomentar valores e instaurarlos como principios, trabajar en formación cívica, promover y defender las libertades individuales y los derechos humanos en Venezuela, sensibilizar a la gente sobre la importancia de conocer, respetar y practicar los principios contenidos en la Declaración Universal de Derechos Humanos, asistir a víctimas de actos violentos y persecución, favorecer la adquisición de conocimiento a través de la lectura y trabajar incansablemente en la ayuda humanitaria como gesto de solidaridad y buena voluntad.</p>
+            
+            <p style="color: black; font-size: 20px;"><b>¿Cuáles son las actividades de LEAN?</b></p>
+            <p>LEAN trabaja en el desarrollo y publicación de estudios de investigación sobre temas de interés social, cultural, político y económico, preparación de ponencias para foros y conferencias, presentación en eventos especializados y mesas de trabajo, petición de colaboración a personalidades de reconocida trayectoria, estudio de casos de violación de derechos humanos a través de letrados voluntarios, asistencia y representación para la defensa de las víctimas de actos violentos y persecución, lanzamiento de campañas sobre valores cívicos y derechos humanos, planificación y ejecución de programas de voluntariado para brindar ayuda humanitaria y organización de charlas sobre la situación económica, política y social de Venezuela y demás temas de interés mundial.</p>
+            
+            <p style="color: black; font-size: 20px;"><b>¿Qu hacemos?</b></p>
+            <p>La asociación civil LEAN fue creada con el objetivo de ayudar, a través de acciones concretas, a nuestros conciudadanos en Venezuela ante la grave escasez de medicinas e insumos médicos en que se encuentra el país. Nuestra misión consiste en recolectar ayuda médico sanitaria en delegaciones en España y, a través de agentes de transporte, llevarlos a Venezuela para que otras asociaciones se encarguen de su distribución. De esta manera aportamos nuestro granito de arena ayudando a llevar asistencia humanitaria a Venezuela. Somos una asociación sin fines de lucro, dedicada a la defensa de los Derechos Humanos.</p>
+
+    </div>
+  </main>
+
+  <footer style="top:200px">
+ <div class="subscribe">
+      <form class="subscribe__item form" id="form" method="POST">
+        <div class='register-text'><i class="fas fa-paper-plane" ></i> Registrese para recibir un <br/> bolitin </div>
+        <input class="form__email" id="email" type="email" name="email" placeholder="Introduce tu correo electronico" ><a><input class="button_theme_dark" id="submit" type="submit" value="Suscribir"></a>
+      </form>
+    </div>
+    <div class="social">
+        <div id="fheading"><h4 font-weight:bold"><span>LEAN EN LAS REDES SOCIALES</span></h4></div>
+        <div class="icons">
+      <a href="#"> 
+<i class="fab fa-twitter" style="font-size:24px;color:#FFC300"></i> </a>
+ <a href="#"> <i class="fab fa-facebook"  style="font-size:24px;color:#FFC300"></i></a> 
+ <a href="#"> <i class="fab fa-instagram"style="font-size:24px;color:#FFC300"></i></a> 
+    </div>
+    <div id="bottom"><p><small>Copyright @2019 All rights reserved | This web is made with <i class="far fa-heart"></i> by <span style="color: #FFC300">DiazApps</span></small></p></div>
+    </div>
+
+</div>
+</footer>
+
+
+</div>
+</body>
+</html>
