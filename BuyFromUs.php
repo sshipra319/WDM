7fce2440c64b3fdc8e70616eb3dabba208ae5413
sshipra319 <56846583+sshipra319@users.noreply.github.com>
2020-10-20 12:57:43 -0500

    Add files via upload

---
+++ BuyFromUs.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="CSS/leanevent.css">
+	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+    <link rel="icon" href="imagenes/favicon.ico" type="image/x-icon" />
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<title>Buy From Us</title>
+</head>
+
+<body>
+	<div id="wrapper">
+<header>
+  <nav>
+    <img src="imagenes\logo-blanco.png" alt="Lean logo" class="hidden-xs" />
+    <div id="logoName">
+      <h3>LEANEVENTO</h3>
+    </div>
+    <ul>
+      <li>
+        <a href="Index.html">Inicio</a>
+        <a href="AboutUs">Quienes Somos</a>
+        <a href="Blog.html">Blog</a>
+        <a href="SignUp.html">Registrate</a>
+        <a href="Contact.html">Contacto</a>
+        <a href="Login.html">Iniciar Sesion</a>
+        <a href="BuyFromUs.html" id="active">Comprar Boletos</a>
+      </li>
+    </ul>
+  </nav>
+</header>
+
+<main>
+  <div class= "container-image ">
+    <img src="imagenes\bannercboleto.jpg"/>
+    <h1 class="centre"> COMPRAR BOLETOS </h1>
+		<div class="below"><span style="color: #FFC300;">INCIO &nbsp;&nbsp;</span> COMPRAR BOLETOS </div>
+  </div>
+
+  <div id="events">
+     <h2>NUESTROS EVENTOS</h2>
+     <p> Tu asistencia es importante para nosotros visitanos en los eventos qu estamos realizando.</p>
+  </div>
+
+  <div id="sell">
+    <div class="cards">
+        <div class="card">
+           <img src="imagenes\minibaner4.jpg">
+    <h5>NO PERDAMOS LA FE <br> <span style="color: #FFC300; line-height: 2.5rem; "> <a href="BuyFromUs2ndPage.html" style="color: #FFC300;"> $300.00 </a></span></h5>
+        </div>
+        
+        <div class="card">
+           <img src="imagenes\minibaner1.jpg" >
+           <h5>LA IMPORTANCIA DE LOS ALIMENTOS <br> <span style="color: #FFC300; line-height: 2.5rem; ">$300.00</span></h5>
+        </div>
+        
+        <div class="card">
+           <img src="imagenes\minibaner2.jpg">
+           <h5>EDUCANDO PARA EL FUTURO <br> <span style="color: #FFC300; line-height: 2.5rem; ">Entrada Gratis</span></h5>
+        </div>
+        
+        <div class="card">
+           <img src="imagenes\minibaner3.jpg" >
+           <h5>POR UNA SONRISA DE VIDA <br> <span style="color: #FFC300; line-height: 2.5rem; ">$300.00</span></h5>
+        </div>
+     </div>
+  </div>
+</main>
+
+<footer style="top:200px">
+ <div class="subscribe">
+      <form class="subscribe__item form" id="form" method="POST">
+        <div class='register-text'><i class="fas fa-paper-plane"></i> Registrese para recibir un <br/> bolitin </div>
+        <input class="form__email" id="email" type="email" name="email" placeholder="Introduce tu correo electronico" ><a><input class="button_theme_dark" id="submit" type="submit" value="Suscribir"></a>
+      </form>
+    </div>
+    <div class="social">
+        <div id="fheading"><h4 font-weight:bold"><span>LEAN EN LAS REDES SOCIALES</span></h4></div>
+        <div class="icons">
+      <a href="#"> 
+<i class="fab fa-twitter" style="font-size:24px;color:#FFC300"></i> </a>
+ <a href="#"> <i class="fab fa-facebook"  style="font-size:24px;color:#FFC300"></i></a> 
+ <a href="#"> <i class="fab fa-instagram"style="font-size:24px;color:#FFC300"></i></a> 
+    </div>
+    <div id="bottom"><p><small>Copyright @2019 All rights reserved | This web is made with <i class="far fa-heart"></i> by <span style="color: #FFC300">DiazApps</span></small></p></div>
+    </div>
+</div>
+</footer>
+
+</div>
+</body>
+</html>
+
+
