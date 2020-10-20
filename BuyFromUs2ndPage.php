e355335447a292b5e3b3c1c43377298d6270f60d
sshipra319 <56846583+sshipra319@users.noreply.github.com>
2020-10-20 12:59:10 -0500

    Add files via upload

---
+++ BuyFromUs2ndPage.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="CSS/leanevent.css">
+	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+    <link rel="icon" href="imagenes/favicon.ico" type="image/x-icon" />
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<title>Buy From Us</title>
+</head>
+
+<body>
+	<div id="wrapper">
+<header>
+  <nav>
+    <img src="imagenes\logo-blanco.png" alt="Lean logo" class="hidden-xs" />
+    <div id="logoName">
+      <h3>LEANEVENTO</h3>
+    </div>
+    <ul>
+      <li>
+        <a href="Index.html">Inicio</a>
+        <a href="AboutUs">Quienes Somos</a>
+        <a href="Blog.html">Blog</a>
+        <a href="SignUp.html">Registrate</a>
+        <a href="Contact.html">Contacto</a>
+        <a href="Login.html">Iniciar Sesion</a>
+        <a href="BuyFromUs.html" id="active">Comprar Boletos</a>
+      </li>
+    </ul>
+  </nav>
+</header>
+
+<main>
+  <div class= "container-image ">
+    <img src="imagenes\bannercboleto.jpg"/>
+    <h1 class="centre"> COMPRAR BOLETOS </h1>
+		<div class="below"><span style="color: #FFC300;">INCIO &nbsp;&nbsp;</span> COMPRAR BOLETOS </div>
+  </div>
+<br/><br/><br/><br/>
+  <div id="purchase">
+         <div class="wrapper">
+         <img src="imagenes\minibaner4.jpg" height="300px" width="300px">
+         <span>
+            <p style="color: #595959;font-weight: bold;opacity: 1.0;float: left;">NO PERDAMOS LA FE</p>
+            <br><br>
+            <p style="color: #595959;font-weight: bold;opacity: 1.0;float: left; margin-left: -160px;">$300.00</p>  
+            <pre style="font-family: 'Roboto', sans-serif;float: right;"> <i class="fas fa-star" style="color: #FFC300;"></i><i class="fas fa-star" style="color: #FFC300;"></i><i class="fas fa-star" style="color: #FFC300;"></i><i class="fas fa-star" style="color: #FFC300;"></i><i class="fas fa-star-half" style="color: #FFC300;"></i> <span style="color:#595959 ;">(74 ratings)</span></pre>
+            <br>
+            <p style="text-align: left; ">¡La fe no se puede perder JAMAS Es imprescindible para todo en nuestras vidas poco a poco las cosas irán mejorando No cambiaran de la noche a la mañana pero van a cambiar y solo cambiarán si te lo propones. Si hoy tuvimos un mal día nuestra meta es tener un mejor mañana Es básicamente hacer nuestra la frase Hoy no me daré por vencido rep tela todos los dias hazla parte de tu filosofia de vida.</p>
+        
+            <p style="float: left">Numero de Entradas</p><br><br>
+            <div style="float: left;">
+            <input type="button" value="-" class="minus" style="float: left; margin-left: -144px;height: 36px; width: 24px;">
+            <input type="text" step="1" min="1" max="" name="quantity" value="1" title="Qty" class="input-text qty text" size="4" pattern="" inputmode="" style="float: left; margin-left: -120px; height: 34px; width: 118px;margin-top:0px;">
+            <input type="button" value="+" class="plus" style="float: left;height: 36px; width: 24px;margin-left: -2px;">
+            </div><br><br>
+            <button type="submit" class="Comprarbutton"><i class="fa fa-shopping-cart"> </i>Comprar</button>
+        </span>
+        </div>
+        </div>
+
+<br><br><br><br>
+<section id="description">	
+        <div class="wrapper">
+        <div id="table">
+            <div class="row">
+                <div class="cell" style="background-color:#FFC300; color: white;">DESCRIPCION</div>
+                <div class="cell" style="width:0.1px; height: 0.1px;"> </div>
+                <div class="cell" style="background-color:#D3D3D3;">ENCARGADOS</div>
+                <div class="cell" style="width: 0.1px; height: 0.1px;"></div>
+                <div class="cell" style="background-color:#D3D3D3;"> PATROCINANTES</div>
+            </div>
+        </div>
+        </div>
+    </section>
+
+    <br/>
+    <div class="wrapper">
+        <div class="table"  style="border: 1px solid #595959;opacity: 0.5">
+            <div class="row">
+                <div class="cell">
+                    <p>Recién he comenzado a leer un libro cuyo mensaje principal es aprender a buscar ese algo mejor todos los días El libro está escrito por una persona que vive 
+con diabetes tipo 1 y nos presenta como los adelantos en tratamientos y tecnología, aunque no han curado su condición dia tras dia mejoran su calidad de vida.
+                    </p>
+                    <p>Busquemos siempre mejorar algo en nuestras vidas, mantengamos el deseo de progresar, de educamos más acerca de la condición de nuestros hijos y verás como 
+poco  a poco comenzamos a entenderla mejor
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</main>
+
+<footer style="top:200px">
+ <div class="subscribe">
+      <form class="subscribe__item form" id="form" method="POST">
+        <div class='register-text'><i class="fas fa-paper-plane"></i> Registrese para recibir un <br/> bolitin </div>
+        <input class="form__email" id="email" type="email" name="email" placeholder="Introduce tu correo electronico" ><a><input class="button_theme_dark" id="submit" type="submit" value="Suscribir"></a>
+      </form>
+    </div>
+    <div class="social">
+        <div id="fheading"><h4 font-weight:bold"><span>LEAN EN LAS REDES SOCIALES</span></h4></div>
+        <div class="icons">
+      <a href="#"> 
+<i class="fab fa-twitter" style="font-size:24px;color:#FFC300"></i> </a>
+ <a href="#"> <i class="fab fa-facebook"  style="font-size:24px;color:#FFC300"></i></a> 
+ <a href="#"> <i class="fab fa-instagram"style="font-size:24px;color:#FFC300"></i></a> 
+    </div>
+    <div id="bottom"><p><small>Copyright @2019 All rights reserved | This web is made with <i class="far fa-heart"></i> by <span style="color: #FFC300">DiazApps</span></small></p></div>
+    </div>
+
+</div>
+</footer>
+
+</div>
+</body>
+</html>
