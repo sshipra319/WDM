1801a96e2c0e82aeba05dd48ad12522eb3002a56
sshipra319 <56846583+sshipra319@users.noreply.github.com>
2020-10-27 00:21:31 -0500

    Add files via upload

---
+++ SignUp.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="CSS/leanevent.css">
+	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+    <link rel="icon" href="imagenes/favicon.ico" type="image/x-icon" />
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<title>Sign Up</title>		
+	</head>
+	<body>
+		<div id="wrapper">
+<header id="header">
+  <nav id="nav-bar">
+    <img src="imagenes\logo-blanco.png" alt="Lean logo" class="hidden-xs" />
+    <div id="logoName">
+      <h3>LEANEVENTO</h3>
+    </div>
+    <ul>
+      <li>
+        <a href="Index.html">Inicio</a>
+        <a href="AboutUs.html">Quienes Somos</a>
+        <a href="Blog.html">Blog</a>
+        <a href="SignUp.html" id="active">Registrate</a>
+        <a href="Contact.html">Contacto</a>
+        <a href="Login.html">Iniciar Sesion</a>
+        <a href="BuyFromUs.html">Comprar Boletos</a></li>
+
+      </li>
+    </ul>
+  </nav>
+</header>
+		<main>
+			<div class= "container-image ">
+    <img src="imagenes\bannerregistro.jpg"/> 
+    <div class="centreSign"> <h1>REGÍSTRATE</h1> </div>
+		<div class="belowSign"><span style="color: #FFC300;">INICIO</span> REGÍSTRATE </div>   
+  </div>
+
+  <div class="SignUp">
+	<h3 style="font-family: Roboto; font-weight: 400;">Elija el tipo de usuario para registrarse</h3><hr style="color: #FFFFFF;"><br/>
+	<a href="Individual.php"> <input type="submitSign" name="individual" value="Como individual" style="width: 100px;"></a>
+	<a href="Business.php"> <input type="submitSign" name="foundation" value="Como Negocio o Fundacion"></a>
+	<a href="Agent.php"> <input type="submitSign" class="button" name="agent" value="Como agente LEAN"></a><br/><br/>
+	
+</div>
+			
+		</main>
+		<footer style="margin-top: -180px;">
+<div class="social">
+        <div id="fheading"><h4 style="font-weight:bold"><span>LEAN EN LAS REDES SOCIALES</span></h4></div>
+        <div class="icons">
+      <a href="#"> 
+ <i class="fa fa-twitter" style="font-size:24px;color:#FFC300"></i> </a>
+ <a href="#"> <i class="fa fa-facebook"  style="font-size:24px;color:#FFC300"></i></a> 
+ <a href="#"> <i class="fa fa-instagram"style="font-size:24px;color:#FFC300"></i></a> 
+    </div><br/><br/><br/>
+    <div id="bottom" style="text-align: center;"><p><small>Copyright @2019 All rights reserved | This web is made with <i class="fa fa-heart-o"></i> by <span style="color: #FFC300">DiazApps</span></small></p></div>
+    </div>	
+    </footer>
+		
+
+
+
+	</body>
+</html>
